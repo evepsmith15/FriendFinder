9e917c3bb260d82759844e73fd55762b9fa253c3
Everett Smith <evepsmith15@gmail.com>
2019-07-09 23:41:46 -0700

    started adding more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,112 @@
-//In here we got friend data 
+//Lists of husbands you might get
 
-var friendsArray [
-{
+var friendsArray[{
     name: 'Corrin',
     photo: '',
-    scores: []
+    scores: [
+        '3',
+        '2',
+        '3',
+        '3',
+        '5',
+        '5',
+        '4',
+        '3',
+        '1',
+        '1'
+    ]
+}, {
+    name: 'Pit',
+    photo: '',
+    scores: [
+        '3',
+        '2',
+        '3',
+        '3',
+        '5',
+        '5',
+        '4',
+        '3',
+        '1',
+        '1'
+    ]
+}, {
+    name: 'Ike',
+    photo: '',
+    scores: [
+        '3',
+        '2',
+        '3',
+        '3',
+        '5',
+        '5',
+        '4',
+        '3',
+        '1',
+        '1'
+    ]
+}, {
+    name: 'Byleth',
+    photo: '',
+    scores: [
+        '3',
+        '2',
+        '3',
+        '3',
+        '5',
+        '5',
+        '4',
+        '3',
+        '1',
+        '1'
+    ]
+},
+{
+    name: 'Dio',
+    photo: '',
+    scores: [
+    '3',
+    '2',
+    '3',
+    '3',
+    '5',
+    '5',
+    '4',
+    '3',
+    '1',
+    '1'
+    ]
+},
+name: 'Kisa',
+    photo: '',
+    scores: [
+    '3',
+    '2',
+    '3',
+    '3',
+    '5',
+    '5',
+    '4',
+    '3',
+    '1',
+    '1'
+    ]
+},
+name: 'Train',
+    photo: '',
+    scores: [
+    '3',
+    '2',
+    '3',
+    '3',
+    '5',
+    '5',
+    '4',
+    '3',
+    '1',
+    '1'
+    ]
 },
 ];
+
 module.exports = friendsArray;
