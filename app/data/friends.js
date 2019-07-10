a27658c1dac17d8ffd0386b2ef608518d1c932cc
Everett Smith <evepsmith15@gmail.com>
2019-07-10 13:51:16 -0700

    adding more stuff to survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,9 +14,72 @@ var friendsArray[{
         '3',
         '1',
         '1'
-    ]
-}, {
-    name: 'Pit',
+    },
+    {
+        name: 'Pit',
+        photo: '',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Ike',
+        photo: '',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Byleth',
+        photo: '',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Dio',
+        photo: '',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
+    },
+    name: 'Kisa',
     photo: '',
     scores: [
         '3',
@@ -28,73 +91,12 @@ var friendsArray[{
         '4',
         '3',
         '1',
-        '1'
-    ]
-}, {
-    name: 'Ike',
-    photo: '',
-    scores: [
-        '3',
-        '2',
-        '3',
-        '3',
-        '5',
-        '5',
-        '4',
-        '3',
-        '1',
-        '1'
-    ]
-}, {
-    name: 'Byleth',
-    photo: '',
-    scores: [
-        '3',
-        '2',
-        '3',
-        '3',
-        '5',
-        '5',
-        '4',
-        '3',
-        '1',
-        '1'
-    ]
-},
-{
-    name: 'Dio',
-    photo: '',
-    scores: [
-    '3',
-    '2',
-    '3',
-    '3',
-    '5',
-    '5',
-    '4',
-    '3',
-    '1',
-    '1'
-    ]
-},
-name: 'Kisa',
-    photo: '',
-    scores: [
-    '3',
-    '2',
-    '3',
-    '3',
-    '5',
-    '5',
-    '4',
-    '3',
-    '1',
-    '1'
+        '4'
     ]
 },
 name: 'Train',
-    photo: '',
-    scores: [
+photo: '',
+scores: [
     '3',
     '2',
     '3',
@@ -105,7 +107,52 @@ name: 'Train',
     '3',
     '1',
     '1'
-    ]
+]
+}, {
+name: 'Ayame',
+photo: '',
+scores: [
+    '3',
+    '2',
+    '3',
+    '3',
+    '5',
+    '5',
+    '4',
+    '3',
+    '1',
+    '1'
+]
+}, {
+name: 'Joker',
+photo: 'https://res.cloudinary.com/cook-becker/image/fetch/q_auto:best,f_auto,w_1920,e_sharpen/https://candb.com/site/candb/images/artwork/Joker_Persona-5_Atlus_1920.jpg',
+scores: [
+    '2',
+    '2',
+    '3',
+    '3',
+    '5',
+    '5',
+    '4',
+    '2',
+    '5',
+    '5'
+]
+}, {
+name: 'Ike',
+photo: '',
+scores: [
+    '3',
+    '2',
+    '3',
+    '3',
+    '5',
+    '5',
+    '4',
+    '3',
+    '1',
+    '1'
+]
 },
 ];
 
