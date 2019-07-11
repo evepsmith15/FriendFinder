9977f9f720fdf2fe502212c48e98548a80aa40b1
Everett Smith <evepsmith15@gmail.com>
2019-07-10 20:52:51 -0700

    friends js works

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,20 @@
 //Lists of husbands you might get
 
-var friendsArray[{
-    name: 'Corrin',
-    photo: '',
-    scores: [
-        '3',
-        '2',
-        '3',
-        '3',
-        '5',
-        '5',
-        '4',
-        '3',
-        '1',
-        '1'
+var friendsArray = [{
+        name: 'Corrin',
+        photo: 'https://vignette.wikia.nocookie.net/fireemblem/images/b/bc/B14-053HN_artwork.png/revision/latest?cb=20180927220130',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
     },
     {
         name: 'Pit',
@@ -32,7 +33,7 @@ var friendsArray[{
         ]
     },
     {
-        name: 'Ike',
+        name: 'Fai',
         photo: '',
         scores: [
             '3',
@@ -79,81 +80,83 @@ var friendsArray[{
             '1'
         ]
     },
-    name: 'Kisa',
-    photo: '',
-    scores: [
-        '3',
-        '2',
-        '3',
-        '3',
-        '5',
-        '5',
-        '4',
-        '3',
-        '1',
-        '4'
-    ]
-},
-name: 'Train',
-photo: '',
-scores: [
-    '3',
-    '2',
-    '3',
-    '3',
-    '5',
-    '5',
-    '4',
-    '3',
-    '1',
-    '1'
-]
-}, {
-name: 'Ayame',
-photo: '',
-scores: [
-    '3',
-    '2',
-    '3',
-    '3',
-    '5',
-    '5',
-    '4',
-    '3',
-    '1',
-    '1'
-]
-}, {
-name: 'Joker',
-photo: 'https://res.cloudinary.com/cook-becker/image/fetch/q_auto:best,f_auto,w_1920,e_sharpen/https://candb.com/site/candb/images/artwork/Joker_Persona-5_Atlus_1920.jpg',
-scores: [
-    '2',
-    '2',
-    '3',
-    '3',
-    '5',
-    '5',
-    '4',
-    '2',
-    '5',
-    '5'
-]
-}, {
-name: 'Ike',
-photo: '',
-scores: [
-    '3',
-    '2',
-    '3',
-    '3',
-    '5',
-    '5',
-    '4',
-    '3',
-    '1',
-    '1'
-]
-},
+    {
+        name: 'Kisa',
+        photo: '',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '4'
+        ]
+    },
+    {
+        name: 'Train',
+        photo: '',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
+    }, {
+        name: 'Ayame',
+        photo: '',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
+    }, {
+        name: 'Joker',
+        photo: 'https://res.cloudinary.com/cook-becker/image/fetch/q_auto:best,f_auto,w_1920,e_sharpen/https://candb.com/site/candb/images/artwork/Joker_Persona-5_Atlus_1920.jpg',
+        scores: [
+            '3',
+            '4',
+            '4',
+            '4',
+            '1',
+            '4',
+            '3',
+            '2',
+            '5',
+            '5'
+        ]
+    }, {
+        name: 'Ike',
+        photo: 'https://vignette.wikia.nocookie.net/fireemblem/images/3/34/B12-003N_artwork.png/revision/latest?cb=20180322010115',
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '3',
+            '1',
+            '1'
+        ]
+    }
 ];
 
 module.exports = friendsArray;
