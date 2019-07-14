b0b329260e4cc33728c4868e88ed26b12889affc
Everett Smith <evepsmith15@gmail.com>
2019-07-14 13:21:51 -0700

    Added all the pictures and fun notes

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ Homework 11
 
 Friend Finder is
 
-Bamazon relies on three javascripts and a mysql to function. A package json and node modules are used to run the program.
+Friend Finger relies on three javascripts and a mysql to function. A package json and node modules are used to run the program.
 
 //What the code does 
 
-Instructions on how to use/install bamazon: I. Make sure visual studio code is downloaded onto your computer. II. Clone this repo (click the clone or download button and copy the link) III. Install the package using the terminal. npm init creates package.JSON. npm install creates the locked package.JSON. IV. Install the node modules. V. The terminal can allow you access to all three javascripts when everything is installed. Customer allows you to buy an item from the product list. Supervisor allows you to add a department and manager allows you to add, restock and delete products.
+Instructions on how to use/install bamazon: 
+I. Make sure visual studio code is downloaded onto your computer. 
+II. Clone this repo (click the clone or download button and copy the link) 
+III. Install the package using the terminal. npm init creates package.JSON. npm install creates the locked package.JSON. 
+IV. Install the node modules. 
+V. The terminal can allow you access to all three javascripts when everything is installed. Customer allows you to buy an item from the product list. Supervisor allows you to add a department and manager allows you to add, restock and delete products.
 
 Technology used: Visual Studio Code: 
 --> Javascript 
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,8 @@
 //Lists of husbands you might get
 
 var friendsArray = [{
-        name: 'Corrin',
+        name: 'Corrin', 
+        //You matched with Corrin. Kind, considerate but on the naive side. He always sees the best in others. 
         photo: 'https://vignette.wikia.nocookie.net/fireemblem/images/b/bc/B14-053HN_artwork.png/revision/latest?cb=20180927220130',
         scores: [
             '3',
@@ -18,7 +19,8 @@ var friendsArray = [{
     },
     {
         name: 'Pit',
-        photo: '',
+        //You matched with Pit. While he's not bright, he's always cheerful and dedicated to those he loves and respects. 
+        photo: 'https://vignette.wikia.nocookie.net/thejusticeworld/images/b/bd/3ds_kidicarus_3_char10_e3.png/revision/latest?cb=20120619142748',
         scores: [
             '3',
             '2',
@@ -34,7 +36,9 @@ var friendsArray = [{
     },
     {
         name: 'Fai',
-        photo: '',
+        //You matched with Fai. The personality he has is fake. When you push his buttons, he's a rather cold person.
+        //Only a few people can open his heart but he hides his motherly nature with forced jokes and fake smiles. 
+        photo: 'https://osmovies.files.wordpress.com/2016/12/tsubasa-chronicle-fye-d-flourite-eyepatch-otaku-house-600x339.jpg',
         scores: [
             '3',
             '2',
@@ -50,6 +54,8 @@ var friendsArray = [{
     },
     {
         name: 'Byleth',
+        //You matched with Byleth. He's stoic, straight to the point but has a warm heart when he opens up. 
+        //He's a teacher guiding you on the right path.
         photo: 'https://vignette.wikia.nocookie.net/fireemblem/images/b/bc/Artwork_Byleth_M.png/revision/latest?cb=20190426054524',
         scores: [
             '3',
@@ -65,8 +71,9 @@ var friendsArray = [{
         ]
     },
     {
-        name: 'Dio',
-        photo: '',
+        name: 'Ladd Russo',
+        //You matched with Ladd. Somehow you wanted an unstable person. Despite being crazy and bloodthirsty, he respects strong natured individuals. 
+        photo: 'https://static1.comicvine.com/uploads/original/11113/111134319/5143165-8428866405-1628_.jpg',
         scores: [
             '3',
             '2',
@@ -82,7 +89,9 @@ var friendsArray = [{
     },
     {
         name: 'Kisa',
-        photo: '',
+        //You matched with Kisa. While happy-go-lucky on the surface, he's a rather moody individual who looks at everything through jaded lens. 
+        //But should you convince him otherwise, he's a great choice. 
+        photo: 'http://www.anime-evo.net/wp-content/uploads/2011/12/00-3.jpg',
         scores: [
             '3',
             '2',
@@ -98,7 +107,9 @@ var friendsArray = [{
     },
     {
         name: 'Train',
-        photo: '',
+        //You matched with Train. Train has two personalities. Cool-headed and focused or playful and sneaky like a cat.
+        //He keeps his cards close to his chest. He values his friendship with others and fights for a glass of milk.
+        photo: 'https://vignette.wikia.nocookie.net/p__/images/3/3e/Image-anime-bc-4cb5a8a.png/revision/latest?cb=20180304202842&path-prefix=protagonist',
         scores: [
             '3',
             '2',
@@ -113,7 +124,9 @@ var friendsArray = [{
         ]
     }, {
         name: 'Ayame',
-        photo: '',
+        //You matched with Ayame. Effiminate, charming and full of himself. He has the power to persuade others into seeing his way. 
+        //His girly appearance should not be underestimated. He's as smart as a snake and will take advantage of it.
+        photo: 'https://pillowfortmedia.s3.amazonaws.com/posts/aeac68cc198b_%5BHorribleSubs%5D%20Fruits%20Basket%20%282019%29%20-%2013%20%5B1080p%5D.mkv_snapshot_08.09_%5B2019.06.29_13.27.32%5D_stitchB.jpg',
         scores: [
             '3',
             '2',
@@ -128,6 +141,7 @@ var friendsArray = [{
         ]
     }, {
         name: 'Joker',
+        //You matched with Joker. Cunning, charismatic and stylish. This thief stole your heart. 
         photo: 'https://res.cloudinary.com/cook-becker/image/fetch/q_auto:best,f_auto,w_1920,e_sharpen/https://candb.com/site/candb/images/artwork/Joker_Persona-5_Atlus_1920.jpg',
         scores: [
             '3',
@@ -143,6 +157,8 @@ var friendsArray = [{
         ]
     }, {
         name: 'Ike',
+        //You matched with Ike. A mercenary who speaks what comes to mind and is an honest man. 
+        //He always does what he thinks is right. He fights against racism. 
         photo: 'https://vignette.wikia.nocookie.net/fireemblem/images/3/34/B12-003N_artwork.png/revision/latest?cb=20180322010115',
         scores: [
             '3',
